c7a3556755ee212ea1ae3ab671dd9f3e5c141b99
remeessalam <remeesreme4u@gmail.com>
2025-02-06 17:40:17 +0530

    landing page header hangburger changed

--- src/components/LandingPage/Header.jsx
+++ src/components/LandingPage/Header.jsx
@@ -81,7 +81,7 @@ const Header = () => {
             onClick={() => setIsOpen(!isOpen)}
           >
             <Hamburger
-              color="#17012C"
+              color="#ffff"
               size="26"
               toggled={isOpen}
               rounded
